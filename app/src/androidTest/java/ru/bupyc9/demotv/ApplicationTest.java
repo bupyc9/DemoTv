d292ac549fa1bea2bde3e1d7fe428742f8e914ba
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-06-21 09:29:36 +0300

    Initial commit

---
+++ app/src/androidTest/java/ru/bupyc9/demotv/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ru.bupyc9.demotv;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
