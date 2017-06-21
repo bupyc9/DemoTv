cf94fc10e00b63f987755edb2e5eae9794a563f4
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-06-21 12:40:32 +0300

    Удалена зависимость от библиотеки appcompat

--- app/src/main/java/ru/bupyc9/demotv/TvActivity.kt
+++ app/src/main/java/ru/bupyc9/demotv/TvActivity.kt
@@ -1,9 +1,9 @@
 package ru.bupyc9.demotv
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.FragmentActivity
 
-class TvActivity : AppCompatActivity() {
+class TvActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
