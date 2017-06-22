9f47130215bc24e2e72166747df8d7714ab6a771
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-06-22 10:12:15 +0300

    Construction of BrowseFragment

---
+++ app/src/main/java/ru/bupyc9/demotv/ui/GridItemPresenter.kt
@@ -0,0 +1,33 @@
+package ru.bupyc9.demotv.ui
+
+import android.graphics.Color
+import android.support.v17.leanback.widget.Presenter
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.TextView
+import ru.bupyc9.demotv.R
+
+class GridItemPresenter: Presenter() {
+    private val GRID_ITEM_WIDTH: Int = 300
+    private val GRID_ITEM_HEIGHT: Int = 200
+
+    override fun onCreateViewHolder(parent: ViewGroup?): ViewHolder {
+        val view: TextView = TextView(parent!!.context)
+        view.layoutParams = ViewGroup.LayoutParams(GRID_ITEM_WIDTH, GRID_ITEM_HEIGHT)
+        view.isFocusable = true
+        view.isFocusableInTouchMode = true
+        view.setBackgroundColor(parent.resources.getColor(R.color.default_background))
+        view.setTextColor(Color.WHITE)
+        view.gravity = Gravity.CENTER
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder?, item: Any?) {
+        (viewHolder!!.view as TextView).text = item as String
+    }
+
+    override fun onUnbindViewHolder(viewHolder: ViewHolder?) {
+
+    }
+}
--- app/src/main/java/ru/bupyc9/demotv/ui/MainFragment.kt
+++ app/src/main/java/ru/bupyc9/demotv/ui/MainFragment.kt
@@ -2,6 +2,10 @@ package ru.bupyc9.demotv.ui
 
 import android.os.Bundle
 import android.support.v17.leanback.app.BrowseFragment
+import android.support.v17.leanback.widget.ArrayObjectAdapter
+import android.support.v17.leanback.widget.HeaderItem
+import android.support.v17.leanback.widget.ListRow
+import android.support.v17.leanback.widget.ListRowPresenter
 import android.util.Log
 import ru.bupyc9.demotv.R
 
@@ -9,12 +13,15 @@ import ru.bupyc9.demotv.R
 class MainFragment: BrowseFragment() {
     private val TAG: String = MainFragment::class.java.simpleName
 
+    private lateinit var mRowsAdapter: ArrayObjectAdapter
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         Log.i(TAG, "onActivityCreated")
 
         setUIElements()
+        loadRows()
     }
 
     private fun setUIElements() {
@@ -23,5 +30,18 @@ class MainFragment: BrowseFragment() {
         isHeadersTransitionOnBackEnabled = true
         brandColor = resources.getColor(R.color.fastlane_background)
         searchAffordanceColor = resources.getColor(R.color.search_opaque)
+    }
+
+    private fun loadRows() {
+        mRowsAdapter = ArrayObjectAdapter(ListRowPresenter())
+        val gridItemPresenterHeader = HeaderItem(0, "GridItemPresenter")
+        val gridPresenter = GridItemPresenter()
+        val gridRowAdapter = ArrayObjectAdapter(gridPresenter)
+        gridRowAdapter.add("ITEM 1")
+        gridRowAdapter.add("ITEM 2")
+        gridRowAdapter.add("ITEM 3")
+        mRowsAdapter.add(ListRow(gridItemPresenterHeader, gridRowAdapter))
+
+        adapter = mRowsAdapter
     }
 }
