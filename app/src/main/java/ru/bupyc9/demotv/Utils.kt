b9c21bebf283a0931e65d7f4f2a891dfa710fdbe
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-06-23 09:59:39 +0300

    Delete companion object on Utils

--- app/src/main/java/ru/bupyc9/demotv/Utils.kt
+++ app/src/main/java/ru/bupyc9/demotv/Utils.kt
@@ -5,52 +5,48 @@ import android.graphics.Point
 import android.view.WindowManager
 import android.widget.Toast
 
-class Utils {
-    companion object {
-        @JvmStatic fun getDisplaySize(context: Context): Point {
-            val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-            val display = windowManager.defaultDisplay
-            val size = Point()
-            display.getSize(size)
+fun getDisplaySize(context: Context): Point {
+    val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    val display = windowManager.defaultDisplay
+    val size = Point()
+    display.getSize(size)
 
-            return size
-        }
+    return size
+}
 
-        @JvmStatic fun showToast(context: Context, message: String) {
-            Toast.makeText(context, message, Toast.LENGTH_LONG).show()
-        }
+fun showToast(context: Context, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+}
 
-        @JvmStatic fun convertDpToPixel(context: Context, dp: Int): Int {
-            val density = context.resources.displayMetrics.density
+fun convertDpToPixel(context: Context, dp: Int): Int {
+    val density = context.resources.displayMetrics.density
 
-            return  Math.round(dp * density)
-        }
+    return  Math.round(dp * density)
+}
 
-        @JvmStatic fun formatMillis(millis: Int): String {
-            var m = millis
-            var result = ""
-            val hr = m / 3600000
-            m %= 3600000
-            val min = m / 60000
-            m %= 60000
-            val sec = m / 1000
-            if (hr > 0) {
-                result += hr.toString() + ":"
-            }
-            if (min >= 0) {
-                if (min > 9) {
-                    result += min.toString() + ":"
-                } else {
-                    result += "0$min:"
-                }
-            }
-            if (sec > 9) {
-                result += sec
-            } else {
-                result += "0" + sec
-            }
-
-            return result
+fun formatMillis(millis: Int): String {
+    var m = millis
+    var result = ""
+    val hr = m / 3600000
+    m %= 3600000
+    val min = m / 60000
+    m %= 60000
+    val sec = m / 1000
+    if (hr > 0) {
+        result += hr.toString() + ":"
+    }
+    if (min >= 0) {
+        if (min > 9) {
+            result += min.toString() + ":"
+        } else {
+            result += "0$min:"
         }
     }
+    if (sec > 9) {
+        result += sec
+    } else {
+        result += "0" + sec
+    }
+
+    return result
 }
