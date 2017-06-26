864f28458b345cf0e97deebcbfc712b1f88b42ae
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-06-26 11:59:21 +0300

    Extend Movie from Parcelable

--- app/src/main/java/ru/bupyc9/demotv/model/Movie.kt
+++ app/src/main/java/ru/bupyc9/demotv/model/Movie.kt
@@ -1,14 +1,44 @@
 package ru.bupyc9.demotv.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import java.net.URI
 import java.net.URISyntaxException
 
-data class Movie(val id: Int, val title: String, val studio: String, val imageUrl: String) {
+data class Movie(
+        val id: Int,
+        val title: String,
+        val studio: String,
+        val imageUrl: String
+) : Parcelable {
     fun getImageURI(): URI? {
         try {
             return URI(imageUrl)
         } catch (e: URISyntaxException) {
             return null
         }
+    }
+
+    companion object {
+        @JvmField val CREATOR: Parcelable.Creator<Movie> = object : Parcelable.Creator<Movie> {
+            override fun createFromParcel(source: Parcel): Movie = Movie(source)
+            override fun newArray(size: Int): Array<Movie?> = arrayOfNulls(size)
+        }
+    }
+
+    constructor(source: Parcel) : this(
+            source.readInt(),
+            source.readString(),
+            source.readString(),
+            source.readString()
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeInt(id)
+        dest.writeString(title)
+        dest.writeString(studio)
+        dest.writeString(imageUrl)
     }
 }
