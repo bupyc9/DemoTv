0f403f594381200fce994cb342c8c669c6ff41c0
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-07-07 11:34:42 +0300

    Фильтрация поиска по названию и описанию видео

--- app/src/main/java/ru/bupyc9/demotv/ui/search/SearchFragment.kt
+++ app/src/main/java/ru/bupyc9/demotv/ui/search/SearchFragment.kt
@@ -3,6 +3,7 @@ package ru.bupyc9.demotv.ui.search
 import android.Manifest
 import android.content.ActivityNotFoundException
 import android.os.Bundle
+import android.os.Handler
 import android.support.v17.leanback.app.SearchFragment
 import android.support.v17.leanback.widget.*
 import android.util.Log
@@ -11,14 +12,21 @@ import ru.bupyc9.demotv.hasPermission
 import ru.bupyc9.demotv.model.Movie
 import ru.bupyc9.demotv.presenter.CardPresenter
 import ru.bupyc9.demotv.ui.detail.VideoDetailsFragment
+import java.util.*
+import android.text.TextUtils
+
+
 
 class SearchFragment: SearchFragment(), SearchFragment.SearchResultProvider {
     companion object {
-
         @JvmStatic private val TAG = SearchFragment::class.java.simpleName
         @JvmStatic private val REQUEST_SPEECH = 0x00000010
+        @JvmStatic private val SEARCH_DELAY_MS = 1000L
     }
     private lateinit var mRowsAdapter: ArrayObjectAdapter
+    private val mHandler = Handler()
+    private val mDelayedLoad = Runnable { loadRows() }
+    private lateinit var mQuery: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,23 +57,50 @@ class SearchFragment: SearchFragment(), SearchFragment.SearchResultProvider {
 
     override fun getResultsAdapter(): ObjectAdapter {
         Log.d(TAG, "getResultsAdapter");
-        Log.d(TAG, mRowsAdapter.toString());
-
-        val listRowAdapter = ArrayObjectAdapter(CardPresenter())
-        listRowAdapter.addAll(0, MovieProvider.instance.getItems())
-        val header = HeaderItem("Search results")
-        mRowsAdapter.add(ListRow(header, listRowAdapter))
 
         return mRowsAdapter
     }
 
     override fun onQueryTextSubmit(query: String?): Boolean {
-        Log.i(TAG, "Search Query Text Change $query");
+        Log.i(TAG, "Search Query Text Submit $query");
+        loadQueryWithDelay(query!!, 0)
+
         return true;
     }
 
     override fun onQueryTextChange(newQuery: String?): Boolean {
-        Log.i(TAG, "Search Query Text Submit $newQuery");
+        Log.i(TAG, "Search Query Text Change $newQuery");
+        loadQueryWithDelay(newQuery!!, SEARCH_DELAY_MS)
+
         return true;
+    }
+
+    private fun loadRows() {
+        Log.d(TAG, "loadRows")
+
+        mRowsAdapter.clear()
+        val items = mutableListOf<Movie>()
+
+        MovieProvider.instance.getItems().forEach {
+            if (
+            it.title.toLowerCase(Locale.ENGLISH).contains(mQuery.toLowerCase(Locale.ENGLISH))
+                    || it.description.toLowerCase(Locale.ENGLISH).contains(mQuery.toLowerCase(Locale.ENGLISH))
+                    ) {
+                items.add(it)
+            }
+        }
+
+        val listRowAdapter = ArrayObjectAdapter(CardPresenter())
+        listRowAdapter.addAll(0, items)
+        val header = HeaderItem("Search results")
+        mRowsAdapter.add(ListRow(header, listRowAdapter))
+    }
+
+    private fun loadQueryWithDelay(query: String, delay: Long) {
+        mHandler.removeCallbacks(mDelayedLoad)
+        if (!TextUtils.isEmpty(query) && query != "nil") {
+            mQuery = query
+            mHandler.postDelayed(mDelayedLoad, delay)
+        }
     }
 }
